58ab5b31e16e4e98b3ef329f743cca34e7a63a33
Akshat Kumar <akshatkumar.dev@gmail.com>
2020-03-20 17:32:07 +0530

    initial commit with axios and navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="navbar">
+        <ul class="navbar-left-items">
+            <li class="navbar-title"><h2>Playlist Runtime</h2></li>
+            <li class="navbar-item">Runtime</li>
+            <li class="navbar-item">Downloads</li>
+        </ul>        
+    </div>
+</template>
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+<style scoped>
+    .navbar{
+        background-color: lightseagreen;
+        }
+    .navbar-left-items{
+        color: white;
+        display: flex;
+        list-style: none;
+        align-items: center;
+    }
+    .navbar-item,.navbar-title{
+        margin: 0.5rem;
+        padding: 0.5rem;
+        }
+</style>
